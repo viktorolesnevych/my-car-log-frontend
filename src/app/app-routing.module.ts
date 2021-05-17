1f77e99996a58cf8d0bb184997d1a15b1a049f30
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-05-17 11:15:44 -0500

    Signup template added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './components/authorization/login/login.component';
+import {SignupComponent} from './components/authorization/signup/signup.component';
 
 const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
   {
     path: 'login',
     component: LoginComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/authorization/login//login.component';
+import { SignupComponent } from './components/authorization/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
