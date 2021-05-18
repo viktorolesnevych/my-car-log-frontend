4f763c519984f2b199458517c2518a576ac43193
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-05-18 10:06:58 -0500

    Styling models component

--- src/app/models/Model.ts
+++ src/app/models/Model.ts
@@ -2,6 +2,7 @@ export interface Model{
   id?: number;
   brand_id: number;
   name: string;
-  model_class: string;
-  img_link: string;
+  modelClass: string;
+  imgLink: string;
+  vehicleList: any;
 }
