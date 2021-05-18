ae53bab1e16b468ff7f5a7d17d646d8f29553090
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-05-18 16:38:57 -0500

    Working on vehicle component

--- src/app/components/vehicle/vehicle.component.ts
+++ src/app/components/vehicle/vehicle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {VehiclesService} from '../../services/vehicles/vehicles.service';
+import {ActivatedRoute} from "@angular/router";
+import {Vehicle} from "../../models/Vehicle";
 
 @Component({
   selector: 'app-vehicle',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle.component.css']
 })
 export class VehicleComponent implements OnInit {
-
-  constructor() { }
+  vehicle: Vehicle;
+  constructor(private route: ActivatedRoute, private vehicleService: VehiclesService) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+      .subscribe(params => {
+        this.vehicleService.getVehicleById(parseInt(params.get('vehicle-id'), 10)).subscribe((response: Vehicle) =>
+          this.vehicle = response);
+      });
   }
 
 }
